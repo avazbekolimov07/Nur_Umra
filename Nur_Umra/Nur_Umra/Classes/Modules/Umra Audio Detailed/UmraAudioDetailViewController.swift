2e90c143c22e5c02920b0dcc64cad346a6e55b9b
avazbekolimov07 <avazbekolimov007@gmail.com>
2023-02-27 14:30:06 +0500

    two modules were created

---
+++ Nur_Umra/Nur_Umra/Classes/Modules/Umra Audio Detailed/UmraAudioDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UmraAudioDetailController.swift
+//  Nur_Umra
+//
+//  Created by AvazbekOS on 27/02/23.
+//
+
+import Foundation
---
+++ Nur_Umra/Nur_Umra/Classes/Modules/Umra Gide Detailed/UmraGideDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UmraGideDetailController.swift
+//  Nur_Umra
+//
+//  Created by AvazbekOS on 27/02/23.
+//
+
+import Foundation
